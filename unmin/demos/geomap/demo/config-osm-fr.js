399799af64064150a6494db47dc4a86b4e334e1f
Shawn Thompson <plansmash@me.com>
2016-07-15 11:41:02 -0400

    Travis build undefined

---
+++ unmin/demos/geomap/demo/config-osm-fr.js
@@ -0,0 +1,17 @@
+﻿///*
+// * @title WET-BOEW Geomap English config file
+// * @overview Example English configuration file for Geomap with OSM basemap
+// * @license wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
+// * @author @jvanulde
+// */
+/*jshint unused:false*/
+var wet_boew_geomap = {
+	basemap: {
+		title:"OSM Map",
+		type:"osm",
+		mapOptions: {
+			center: [ -123, 49 ],
+			zoomLevel: 5
+		}
+	}
+};
